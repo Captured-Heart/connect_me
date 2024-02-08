8698fe01722cb357febf129b7577237f7d58a519
Captured-Heart <knkpozi@gmail.com>
2024-02-08 16:01:13 +0100

    started customizing the Scan Screen

--- lib/src/profile/presentation/screen/qr_code_scan_screen.dart
+++ lib/src/profile/presentation/screen/qr_code_scan_screen.dart
@@ -1,15 +1,17 @@
+import 'dart:math';
+
 import 'package:connect_me/app.dart' hide CameraFacing;
 import 'package:image_picker/image_picker.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-class QrCodeScanScree extends ConsumerStatefulWidget {
-  const QrCodeScanScree({super.key});
+class QrCodeScanScreen extends ConsumerStatefulWidget {
+  const QrCodeScanScreen({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _QrCodeScanScreeState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _QrCodeScanScreenState();
 }
 
-class _QrCodeScanScreeState extends ConsumerState<QrCodeScanScree> {
+class _QrCodeScanScreenState extends ConsumerState<QrCodeScanScreen> {
   BarcodeCapture? barcode;
 
   final MobileScannerController controller = MobileScannerController(
@@ -46,12 +48,21 @@ class _QrCodeScanScreeState extends ConsumerState<QrCodeScanScree> {
   int? numberOfCameras;
   @override
   Widget build(BuildContext context) {
+    final scanWindow = Rect.fromCenter(
+      center: context.center(
+        Offset(
+          1,
+          -context.sizeHeight(0.12),
+        ),
+      ),
+      width: context.sizeWidth(0.65),
+      height: context.sizeHeight(0.35),
+    );
     return Scaffold(
-      // appBar: AppBar(title: const Text('With controller')),
-      backgroundColor: Colors.black,
       body: Builder(
         builder: (context) {
           return Stack(
+            fit: StackFit.expand,
             children: [
               MobileScanner(
                 onScannerStarted: (arguments) {
@@ -60,27 +71,53 @@ class _QrCodeScanScreeState extends ConsumerState<QrCodeScanScree> {
                     setState(() {});
                   }
                 },
+                scanWindow: scanWindow,
                 controller: controller,
                 errorBuilder: (context, error, child) {
+                  inspect(error);
                   return Center(
-                    child: Text(error.errorDetails?.message ?? 'error'),
+                    child: Text(
+                      '${error.errorDetails?.message}\n             Check Camera Permissions!' ??
+                          'error',
+                    ),
                   );
                 },
-                fit: BoxFit.contain,
+                // overlay: Center(
+                //   child: Card(
+                //     color: Colors.red,
+                //     shape: CustomOverlayShape(
+                //       borderColor: Colors.red,
+                //       borderRadius: 10,
+                //       borderLength: 30,
+                //       borderWidth: 10,
+                //       cutOutSize: context.sizeHeight(0.4),
+                //     ),
+                //   ),
+                // ),
+                fit: BoxFit.cover,
                 onDetect: (barcode) {
+                  //TODO: NAVIGATE TO THE PROFILE OTHER SCREEN HERE
                   setState(() {
                     this.barcode = barcode;
                   });
                 },
               ),
               Align(
+                alignment: Alignment.topCenter,
+                child: CustomPaint(
+                  size: Size(context.sizeWidth(1), context.sizeHeight(0.1)),
+                  painter: ScannerOverlay(scanWindow),
+                ),
+              ),
+              Align(
                 alignment: Alignment.bottomCenter,
                 child: Container(
-                  alignment: Alignment.bottomCenter,
+                  alignment: Alignment.center,
                   height: 100,
-                  color: Colors.black.withOpacity(0.4),
+                  // color: Colors.black.withOpacity(0.4),
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       ValueListenableBuilder(
                         valueListenable: controller.hasTorchState,
@@ -112,30 +149,33 @@ class _QrCodeScanScreeState extends ConsumerState<QrCodeScanScree> {
                           );
                         },
                       ),
+                      // IconButton(
+                      //   color: Colors.white,
+                      //   icon: isStarted ? const Icon(Icons.stop) : const Icon(Icons.play_arrow),
+                      //   iconSize: 32.0,
+                      //   onPressed: _startOrStop,
+                      // ),
+                      // Center(
+                      //   child: SizedBox(
+                      //     width: MediaQuery.of(context).size.width - 200,
+                      //     height: 50,
+                      //     child: FittedBox(
+                      //       child: Text(
+                      //         barcode?.barcodes.first.rawValue ?? 'Scan something!',
+                      //         overflow: TextOverflow.fade,
+                      //         style: Theme.of(context)
+                      //             .textTheme
+                      //             .headlineMedium!
+                      //             .copyWith(color: Colors.white),
+                      //       ),
+                      //     ),
+                      //   ),
+                      // ),
+
+//! CAMERA SWITCH
                       IconButton(
-                        color: Colors.white,
-                        icon: isStarted ? const Icon(Icons.stop) : const Icon(Icons.play_arrow),
-                        iconSize: 32.0,
-                        onPressed: _startOrStop,
-                      ),
-                      Center(
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width - 200,
-                          height: 50,
-                          child: FittedBox(
-                            child: Text(
-                              barcode?.barcodes.first.rawValue ?? 'Scan something!',
-                              overflow: TextOverflow.fade,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headlineMedium!
-                                  .copyWith(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        color: Colors.white,
+                        color: AppThemeColorDark.textDark,
+                        tooltip: 'Switch Camera'.hardCodedString,
                         icon: ValueListenableBuilder<CameraFacing>(
                           valueListenable: controller.cameraFacingState,
                           builder: (context, state, child) {
@@ -151,8 +191,10 @@ class _QrCodeScanScreeState extends ConsumerState<QrCodeScanScree> {
                         onPressed:
                             (numberOfCameras ?? 0) < 2 ? null : () => controller.switchCamera(),
                       ),
+                      //! SELECT IMAGE
                       IconButton(
-                        color: Colors.white,
+                        tooltip: 'Select image'.hardCodedString,
+                        color: AppThemeColorDark.textDark,
                         icon: const Icon(Icons.image),
                         iconSize: 32.0,
                         onPressed: () async {
@@ -164,6 +206,8 @@ class _QrCodeScanScreeState extends ConsumerState<QrCodeScanScree> {
                           if (image != null) {
                             if (await controller.analyzeImage(image.path)) {
                               if (!mounted) return;
+
+                              //TODO: WHAT HAPPENS WHEN A BARCODE IS SCANNED FROM IMAGE
                               ScaffoldMessenger.of(context).showSnackBar(
                                 const SnackBar(
                                   content: Text('Barcode found!'),
@@ -171,6 +215,7 @@ class _QrCodeScanScreeState extends ConsumerState<QrCodeScanScree> {
                                 ),
                               );
                             } else {
+                              //TODO: WHAT HAPPENS IF THE BARCODE COULDNT BE ANALYZED
                               if (!mounted) return;
                               ScaffoldMessenger.of(context).showSnackBar(
                                 const SnackBar(
@@ -191,5 +236,235 @@ class _QrCodeScanScreeState extends ConsumerState<QrCodeScanScree> {
         },
       ),
     );
+  }
+}
+
+class CustomOverlayShape extends ShapeBorder {
+  CustomOverlayShape({
+    this.borderColor = Colors.red,
+    this.borderWidth = 3.0,
+    this.overlayColor = const Color.fromRGBO(0, 0, 0, 80),
+    this.borderRadius = 0,
+    this.borderLength = 40,
+    double? cutOutSize,
+    double? cutOutWidth,
+    double? cutOutHeight,
+    this.cutOutBottomOffset = 0,
+  })  : cutOutWidth = cutOutWidth ?? cutOutSize ?? 250,
+        cutOutHeight = cutOutHeight ?? cutOutSize ?? 250 {
+    assert(
+      borderLength <= min(this.cutOutWidth, this.cutOutHeight) / 2 + borderWidth * 2,
+      "Border can't be larger than ${min(this.cutOutWidth, this.cutOutHeight) / 2 + borderWidth * 2}",
+    );
+    assert(
+        (cutOutWidth == null && cutOutHeight == null) ||
+            (cutOutSize == null && cutOutWidth != null && cutOutHeight != null),
+        'Use only cutOutWidth and cutOutHeight or only cutOutSize');
+  }
+
+  final Color borderColor;
+  final double borderWidth;
+  final Color overlayColor;
+  final double borderRadius;
+  final double borderLength;
+  final double cutOutWidth;
+  final double cutOutHeight;
+  final double cutOutBottomOffset;
+
+  @override
+  EdgeInsetsGeometry get dimensions => const EdgeInsets.all(10);
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+    return Path()
+      ..fillType = PathFillType.evenOdd
+      ..addPath(getOuterPath(rect), Offset.zero);
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    Path _getLeftTopPath(Rect rect) {
+      return Path()
+        ..moveTo(rect.left, rect.bottom)
+        ..lineTo(rect.left, rect.top)
+        ..lineTo(rect.right, rect.top);
+    }
+
+    return _getLeftTopPath(rect)
+      ..lineTo(
+        rect.right,
+        rect.bottom,
+      )
+      ..lineTo(
+        rect.left,
+        rect.bottom,
+      )
+      ..lineTo(
+        rect.left,
+        rect.top,
+      );
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+    final width = rect.width;
+    final borderWidthSize = width / 2;
+    final height = rect.height;
+    final borderOffset = borderWidth / 2;
+    final _borderLength = borderLength > min(cutOutHeight, cutOutHeight) / 2 + borderWidth * 2
+        ? borderWidthSize / 2
+        : borderLength;
+    final _cutOutWidth = cutOutWidth < width ? cutOutWidth : width - borderOffset;
+    final _cutOutHeight = cutOutHeight < height ? cutOutHeight : height - borderOffset;
+
+    final backgroundPaint = Paint()
+      ..color = overlayColor
+      ..style = PaintingStyle.fill;
+
+    final borderPaint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = borderWidth;
+
+    final boxPaint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.fill
+      ..blendMode = BlendMode.dstOut;
+
+    final cutOutRect = Rect.fromLTWH(
+      rect.left + width / 2 - _cutOutWidth / 2 + borderOffset,
+      -cutOutBottomOffset + rect.top + height / 2 - _cutOutHeight / 2 + borderOffset,
+      _cutOutWidth - borderOffset * 2,
+      _cutOutHeight - borderOffset * 2,
+    );
+
+    canvas
+      ..saveLayer(
+        rect,
+        backgroundPaint,
+      )
+      ..drawRect(
+        rect,
+        backgroundPaint,
+      )
+      // Draw top right corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.right - _borderLength,
+          cutOutRect.top,
+          cutOutRect.right,
+          cutOutRect.top + _borderLength,
+          topRight: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw top left corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.left,
+          cutOutRect.top,
+          cutOutRect.left + _borderLength,
+          cutOutRect.top + _borderLength,
+          topLeft: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw bottom right corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.right - _borderLength,
+          cutOutRect.bottom - _borderLength,
+          cutOutRect.right,
+          cutOutRect.bottom,
+          bottomRight: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw bottom left corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.left,
+          cutOutRect.bottom - _borderLength,
+          cutOutRect.left + _borderLength,
+          cutOutRect.bottom,
+          bottomLeft: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      ..drawRRect(
+        RRect.fromRectAndRadius(
+          cutOutRect,
+          Radius.circular(borderRadius),
+        ),
+        boxPaint,
+      )
+      ..restore();
+  }
+
+  @override
+  ShapeBorder scale(double t) {
+    return CustomOverlayShape(
+      borderColor: borderColor,
+      borderWidth: borderWidth,
+      overlayColor: overlayColor,
+    );
+  }
+}
+
+class ScannerOverlay extends CustomPainter {
+  ScannerOverlay(this.scanWindow);
+
+  final Rect scanWindow;
+  final double borderRadius = 12.0;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final backgroundPath = Path()..addRect(Rect.largest);
+    final cutoutPath = Path()
+      ..addRRect(
+        RRect.fromRectAndCorners(
+          scanWindow,
+          topLeft: Radius.circular(borderRadius),
+          topRight: Radius.circular(borderRadius),
+          bottomLeft: Radius.circular(borderRadius),
+          bottomRight: Radius.circular(borderRadius),
+        ),
+      );
+
+    final backgroundPaint = Paint()
+      ..color = Colors.black.withOpacity(0.5)
+      ..style = PaintingStyle.fill
+      ..blendMode = BlendMode.dstOut;
+
+    final backgroundWithCutout = Path.combine(
+      PathOperation.difference,
+      backgroundPath,
+      cutoutPath,
+    );
+
+    // Create a Paint object for the white border
+    final borderPaint = Paint()
+      ..color = Colors.black.withOpacity(0.3)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 0.0; // Adjust the border width as needed
+
+    // Calculate the border rectangle with rounded corners
+// Adjust the radius as needed
+    final borderRect = RRect.fromRectAndCorners(
+      scanWindow,
+      topLeft: Radius.circular(borderRadius),
+      topRight: Radius.circular(borderRadius),
+      bottomLeft: Radius.circular(borderRadius),
+      bottomRight: Radius.circular(borderRadius),
+    );
+
+    // Draw the white border
+    canvas.drawPath(backgroundWithCutout, backgroundPaint);
+    canvas.drawRRect(borderRect, borderPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
