efc6b065ea91eca89b011b52ff8d3a889426d439
Captured-Heart <knkpozi@gmail.com>
2024-05-17 20:29:54 +0100

    added analytics and firebase core mock

---
+++ test/analytics_mock.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_analytics_platform_interface/firebase_analytics_platform_interface.dart';
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+typedef Callback = void Function(MethodCall call);
+
+final List<MethodCall> methodCallLog = <MethodCall>[];
+
+void setupFirebaseAnalyticsMocks([Callback? customHandlers]) {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setupFirebaseCoreMocks();
+
+  TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+      .setMockMethodCallHandler(MethodChannelFirebaseAnalytics.channel,
+          (MethodCall methodCall) async {
+    methodCallLog.add(methodCall);
+    switch (methodCall.method) {
+      case 'Analytics#getAppInstanceId':
+        return 'ABCD1234';
+
+      default:
+        return false;
+    }
+  });
+}
